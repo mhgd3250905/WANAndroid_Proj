572a85201a2a0c8fa9321cded45d9087d3e9a649
ksheng <294851575@qq.com>
2019-02-20 23:28:46 +0800

    1.使用webview_flutter库

--- lib/WanAndroid/web_page.dart
+++ lib/WanAndroid/web_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_webview_plugin/flutter_webview_plugin.dart';
 import 'package:douban_movies/WanAndroid/Data/data_article_bean.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class WebPage extends StatelessWidget {
   final String title;
@@ -10,20 +11,37 @@ class WebPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: new WebviewScaffold(
-        url: url,
-        appBar: new AppBar(
-          title:Text(title),
-        ),
-        withZoom: true,
-        withLocalStorage: true,
-        hidden: true,
-        initialChild: new Center(
-          child: new CircularProgressIndicator(),
-        ),
-      ),
+    return new WebView(
+      onWebViewCreated: (WebViewController webViewController) {
+//          _webViewController = webViewController;
+//          _webViewController.addListener(() {
+//            int _scrollY = _webViewController.scrollY.toInt();
+//            if (_scrollY < 480 && _isShowFloatBtn) {
+//              _isShowFloatBtn = false;
+//              setState(() {});
+//            } else if (_scrollY > 480 && !_isShowFloatBtn) {
+//              _isShowFloatBtn = true;
+//              setState(() {});
+//            }
+//          });
+      },
+      initialUrl: url,
+      javascriptMode: JavascriptMode.unrestricted,
     );
+//    return Container(
+//      child: new WebviewScaffold(
+//        url: url,
+//        appBar: new AppBar(
+//          title:Text(title),
+//        ),
+//        withZoom: true,
+//        withLocalStorage: true,
+//        hidden: true,
+//        initialChild: new Center(
+//          child: new CircularProgressIndicator(),
+//        ),
+//      ),
+//    );
   }
 
 }
